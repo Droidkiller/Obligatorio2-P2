d1d29c0fc13c8fa710b5741906479548947dbb66
alvarezaitana <aitanalvarezz@gmail.com>
2025-11-19 22:45:42 -0300

    creacion de todas las ventanas

---
+++ src/model/Movimiento.java
@@ -0,0 +1,35 @@
+/*
+ Aitana Alvarez 340201
+ Francisco Bonanni 299134
+ */
+
+package model;
+
+public class Movimiento {
+    private final int mes;
+    private final Area areaOrigen;
+    private final Area areaDestino;
+    private final Empleado empleado;
+
+    public Movimiento(int mes, Area origen, Area destino, Empleado empleado) {
+        this.mes = mes;
+        this.areaOrigen = origen;
+        this.areaDestino = destino;
+        this.empleado = empleado;
+    }
+
+    public int getMes() { return mes; }
+    public Area getAreaOrigen() { return areaOrigen; }
+    public Area getAreaDestino() { return areaDestino; }
+    public Empleado getEmployee() { return empleado; }
+    
+    @Override
+    public String toString() {
+        return "Movimiento{mes=" + mes +
+               ", origen=" + areaOrigen.getNombre() +
+               ", destino=" + areaDestino.getNombre() +
+               ", empleado=" + empleado.getNombre() +
+               '}';
+    }
+    
+}
---
+++ src/model/archivos/ArchivoLectura.java
@@ -0,0 +1,44 @@
+/*
+ Aitana Alvarez 340201
+ Francisco Bonanni 299134
+ */
+
+package model.archivos;
+
+import java.io.*; 
+import java.nio.file.Paths; 
+import java.util.Scanner;
+
+public class ArchivoLectura {
+    private Scanner in; 
+    private String linea;
+    
+    public ArchivoLectura(String unNombre) { 
+        try { 
+            in = new Scanner(Paths.get(unNombre)); 
+        } 
+        catch (IOException e) { 
+            System.err.println("Error "); 
+            System.exit(1); 
+        } 
+    }
+    
+    public boolean hayMasLineas() { 
+        boolean hay = false; 
+        linea = null; 
+        if (in.hasNext()){ 
+            linea = in.nextLine(); 
+            hay = true; 
+        } 
+        return hay; 
+    }
+    
+    public String linea() { 
+        // devuelve la última linea leida return linea;
+        return linea;
+    } 
+    
+    public void cerrar() { 
+        in.close(); 
+    } 
+}
